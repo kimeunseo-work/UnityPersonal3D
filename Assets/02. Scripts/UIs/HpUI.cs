848f1ecfd80647ab7e1bbe676fc4a28ff785de1e
kimeunseo <kimeunseo.work@gmail.com>
2025-11-11 19:23:28 +0900

    feat: HP바 증감 애니메이션 추가

--- Assets/02. Scripts/UIs/HpUI.cs
+++ Assets/02. Scripts/UIs/HpUI.cs
@@ -10,6 +10,19 @@ public class HpUI : MonoBehaviour
     [SerializeField] Image _hpbar;
     [SerializeField] Image _hpState;
 
+    
+    Coroutine _coroutine;
+    public Coroutine Coroutine
+    {
+        get => _coroutine;
+        set
+        {
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+            _coroutine = value;
+        }
+    }
+
     private void Reset()
     {
         _owner = GetComponentInParent<Player>();
@@ -25,7 +38,20 @@ public class HpUI : MonoBehaviour
     }
 
     private void HandleHpChanged(int hp, int maxHp)
+        => Coroutine = StartCoroutine(FillAmountImageCoroutine(_hpbar, hp, maxHp));
+
+    private IEnumerator FillAmountImageCoroutine(Image target, int currentValue, int maxValue)
     {
-        _hpbar.fillAmount = (float)hp / maxHp;
+        float end = (float)currentValue / maxValue;
+
+        while (!Mathf.Approximately(target.fillAmount, end))
+        {
+            target.fillAmount = Mathf.MoveTowards(target.fillAmount, end, 1f * Time.deltaTime);
+            yield return null;
+        }
+
+        target.fillAmount = end;
+
+        StopCoroutine(Coroutine);
     }
 }
