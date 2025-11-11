60a8d8db0ad59fbaf7321090596a4fd5ec24ef58
kimeunseo <kimeunseo.work@gmail.com>
2025-11-11 09:51:59 +0900

    feat: 플레이어 스탯 데이터 생성

---
+++ Assets/02. Scripts/Characters/SO_BaseData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SO_BaseData : ScriptableObject
+{
+    public int Hp;
+    public int Atk;
+    public int WalkSpeed;
+    public int RunSpeed;
+}
---
+++ Assets/02. Scripts/Characters/SO_PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Status/PlayerStatus")]
+public class SO_PlayerData : SO_BaseData
+{
+    public int Stamina;
+    public int JumpPower;
+    public int JumpMoveSpeed;
+}
