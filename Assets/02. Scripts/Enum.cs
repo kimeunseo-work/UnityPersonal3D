58bcff4b831554674671c0fc3503314680066b5a
kimeunseo <kimeunseo.work@gmail.com>
2025-11-13 04:24:09 +0900

    feat: Enum, WaitforScondsCash 스크립트 추가

---
+++ Assets/02. Scripts/Enum.cs
@@ -0,0 +1,13 @@
+﻿public static class Enum
+{
+    public enum Stats
+    {
+        Hp,
+        MaxHp,
+        Stamina,
+        MaxStamina,
+        Atk,
+        SpeedWeight,
+        JumpWeight,
+    }
+}
---
+++ Assets/02. Scripts/WaitForSecondsCash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class WaitForSecondsCash
+{
+    public static Dictionary<float, WaitForSeconds> WaitForSeconds { get; set; } = new(10);
+
+    public static WaitForSeconds GetWaitForSeconds(float seconds)
+    {
+        if (!WaitForSeconds.ContainsKey(seconds))
+        {
+            var temp = new WaitForSeconds(seconds);
+            WaitForSeconds.Add(seconds, temp);
+
+            return temp;
+        }
+
+        return WaitForSeconds[seconds];
+    }
+}
