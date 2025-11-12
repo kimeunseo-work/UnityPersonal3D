dba561a8157f318236903f9a5016114ea758916e
kimeunseo <kimeunseo.work@gmail.com>
2025-11-12 17:07:08 +0900

    feat: 인벤토리 UI + 슬롯 + 아이템 획득, 기초 구현

---
+++ Assets/02. Scripts/Items/Slot.cs
@@ -0,0 +1,57 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    [SerializeField] Item _item;
+    [SerializeField] Image _image;
+    [SerializeField] Button _btn;
+    [SerializeField] TextMeshProUGUI _countText;
+
+    public int _currentStackCount = 0;
+    public int CurrentStackCount
+    {
+        get => _currentStackCount;
+        set
+        {
+            _currentStackCount = value;
+            _countText.text = value.ToString();
+        }
+    }
+    public Item Item
+    {
+        get => _item;
+        set
+        {
+            _item = value;
+            SetSlotUI();
+        }
+    }
+
+
+
+    private void Start()
+    {
+        _countText.gameObject.SetActive(false);
+        _btn.interactable = false;
+    }
+
+
+
+    private void SetSlotUI()
+    {
+        if (_item == null)
+        {
+            _image.sprite = null;
+            _btn.interactable = false;
+        }
+        else
+        {
+            if (Item is SingleUseItem)
+                _countText.gameObject.SetActive(true);
+            _image.sprite = Item.Icon;
+            _btn.interactable = true;
+        }
+    }
+}
---
+++ Assets/02. Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [Header("InventoryArea")]
+    [SerializeField] Transform ArmorArea;
+    [SerializeField] Transform WeaponArea;
+    [SerializeField] Transform ActiveItemArea;
+    [SerializeField] Transform ItemArea;
+
+    public Item Armor { get; private set; }
+    public Slot ArmorSlot { get; private set; }
+    public Item Weapon { get; private set; }
+    public Slot WeaponSlot { get; private set; }
+    public List<Item> ActiveItems { get; private set; } = new(4);
+    public List<Slot> ActiveItemSlots { get; private set; }
+    public List<Item> Items { get; private set; } = new(28);
+    public List<Slot> Slots { get; private set; }
+
+
+
+    private void Start()
+    {
+        ArmorSlot = ArmorArea.GetComponentInChildren<Slot>();
+        WeaponSlot = WeaponArea.GetComponentInChildren<Slot>();
+        ActiveItemSlots = ActiveItemArea.GetComponentsInChildren<Slot>().ToList();
+        Slots = ItemArea.GetComponentsInChildren<Slot>().ToList();
+    }
+
+
+
+    public void GetItem(Item targetItem)
+    {
+        if (Items.Contains(targetItem))
+        {
+            var index = Items.IndexOf(targetItem);
+            var slot = Slots[index];
+
+            if (Slots[index].Item is SingleUseItem)
+                slot.CurrentStackCount++;
+            else
+                Debug.Log($"[{gameObject.name}] Item is not SingleUse");
+        }
+        else
+        {
+            Items.Add(targetItem);
+            var index = Items.IndexOf(targetItem);
+            Slots[index].Item = targetItem;
+        }
+    }
+
+    public void RemoveItem(Item item)
+    {
+        var index = Items.IndexOf(item);
+        Items.RemoveAt(index);
+        Slots[index].Item = null;
+    }
+}
